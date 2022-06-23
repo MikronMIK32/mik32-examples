9cc559bd1eb5530e461184c726bbd5d35b672e25
matyar-00 <matyar000@gmail.com>
2022-06-23 15:49:50 +0300

    Отредактирован I2C_S и I2C_M

--- I2C_S/src/main.c
+++ I2C_S/src/main.c
@@ -1,18 +1,18 @@
 #include "common.h"
-#include "i2c_loop_common.h"
+#include "i2c.h"
 #include "stdbool.h"
-#include "wakeup.h"
 
-void i2c_init_slave(I2C_TypeDef* i2c, uint8_t slave_adres)
+
+void i2c_slave_init(I2C_TypeDef* i2c, uint8_t slave_adres)
 {
-    //Включаем тактирование необходимых блоков - GPIO_0, GPIO_1, GPIO_2 и модуля выбора режима GPIO 
+    slave_adres = slave_adres << 1;
+
+    // Включаем тактирование необходимых блоков
     PM->CLK_APB_P_SET |= PM_CLOCK_GPIO_0_M | PM_CLOCK_I2C_0_M;
     PM->CLK_APB_M_SET |= PM_CLOCK_PAD_CONFIG_M | PM_CLOCK_WU_M | PM_CLOCK_PM_M ;
     
     // обнуление регистра управления
     i2c->CR1 = 0;
-
-    // почему то тут нет #define I2C_OAR1_OA1(v)             (((v) << I2C_OAR1_OA1_S) & I2C_OAR1_OA1_M)
 
     /*
     *
@@ -54,135 +54,167 @@ void i2c_init_slave(I2C_TypeDef* i2c, uint8_t slave_adres)
     * CR1 - Регистр управления
     * 
     * PE - Управление интерфейсом: 0 – интерфейс выключен; 1 – интерфейс включен
-    * 
-    * STOPIE - Разрешение прерывания детектировании STOP: 0 – прерывание запрещено; 1 – прерывание разрешено
     *
     */
-    //i2c->CR1 = I2C_CR1_NOSTRETCH_M;
     i2c->CR1 |= I2C_CR1_PE_M;
 
     xprintf("\nВедомый. Старт\n");
 
 }
 
-void i2c_slave_write(I2C_TypeDef* i2c, uint8_t data)
+void i2c_slave_restart(I2C_TypeDef* i2c)
+{
+    // Получение адреса ведомого
+    uint8_t slave_adres = (uint8_t)(i2c->OAR1 & (0b1111111111));
+    slave_adres >>= 1;
+    xprintf("Рестарт. adres = 0x%02x\n", slave_adres);
+    
+    // Программный сброс модуля i2c
+    i2c->CR1 &= ~I2C_CR1_PE_M;
+    for (volatile int i = 0; i < 1000000; i++); 
+
+    // Повторная инициализация
+    i2c_slave_init(i2c, slave_adres);
+}
+
+void i2c_slave_write(I2C_TypeDef* i2c, uint8_t data[], uint8_t byte_count)
 {
 
-    xprintf("Отправка %d\n", data);
-    //xprintf("До цикла TXIS = %d, NACKF = %d, STOPF = %d\n", (i2c->ISR & I2C_ISR_TXIS_M)>>I2C_ISR_TXIS_S, (i2c->ISR & I2C_ISR_NACKF_M)>>I2C_ISR_NACKF_S ,(i2c->ISR & I2C_ISR_STOPF_M)>>I2C_ISR_STOPF_S);
-    while(!(i2c->ISR & I2C_ISR_TXIS_M)) // TXIS = 1 - регистр TXDR свободен
+    xprintf("\nОтправка %d\n", data[0]<<8 | data[1]);
+
+    for (uint8_t i = 0; i < byte_count; i++)
     {
-            //xprintf("В цикле TXIS = %d, NACKF = %d, STOPF = %d\n", (i2c->ISR & I2C_ISR_TXIS_M)>>I2C_ISR_TXIS_S, (i2c->ISR & I2C_ISR_NACKF_M)>>I2C_ISR_NACKF_S ,(i2c->ISR & I2C_ISR_STOPF_M)>>I2C_ISR_STOPF_S);
-            if(i2c->ISR & I2C_ISR_NACKF_M)//при записи байта мастер прислал stop
+        int counter = 0; // Счетчик для ожидания
+        while(!(i2c->ISR & I2C_ISR_TXIS_M)) // TXIS = 1 - регистр TXDR свободен
+        {
+            counter++;
+            if(counter == 1000000)
+            {
+                // Ожидание превышено. Возможно механическое повреждение линии связи
+                break;
+            }
+
+            // При записи байта мастер прислал NACK. Возникла ошибка при передаче
+            if(i2c->ISR & I2C_ISR_NACKF_M)
             {
                 xprintf("Запись. NACKF = %d\n", (i2c->ISR & I2C_ISR_NACKF_M) >> I2C_ISR_NACKF_S);
                 break;
             }
-    } 
+        }
 
-    if(i2c->ISR & I2C_ISR_NACKF_M)//при записи байта мастер прислал STOP
-    {
-        i2c->ICR |= I2C_ICR_STOPCF_M | I2C_ICR_NACKCF_M; // сброс флага STOPF и NACKF
-        xprintf("Сброс STOPF и NACKF\n");
-        //break;
-        return;
-    }
-
-    //xprintf("Загрузка %d в TXDR\n", data);
-    i2c->TXDR = data; // первая загрузка в буфер
-    xprintf("Отправлено %d\n", data);
-    i2c->ICR |= I2C_ICR_STOPCF_M | I2C_ICR_NACKCF_M; // сброс флага STOPF
-
-}
-
-uint8_t i2c_slave_read(I2C_TypeDef* i2c)
-{
-    uint8_t data = 0;
-    uint8_t counter = 0;
-
-    //байт нужно прочитать когда RXNE = 1
-    while(!(i2c->ISR & I2C_ISR_RXNE_M))
-    {
-        //xprintf("RXNE = %d\n", (i2c->ISR & I2C_ISR_RXNE_M)>>I2C_ISR_RXNE_S);
-
-        // counter++;
-        // if(counter>100)
-        // break;
-
-        if((i2c->ISR & I2C_ISR_STOPF_M))
+        if(counter == 1000000)
         {
-            xprintf("Чтение. STOPF = %d, NACKF = %d\n", (i2c->ISR & I2C_ISR_STOPF_M) >> I2C_ISR_STOPF_S, (i2c->ISR & I2C_ISR_NACKF_M) >> I2C_ISR_NACKF_S);
+            xprintf("Разрыв связи\n");
+
+            // Программный сброс модуля i2c и его повторная инициалиизация 
+            i2c_slave_restart(i2c);
+            break;
+        } 
+
+        // При записи байта мастер прислал NACK. Возникла ошибка при передаче
+        if(i2c->ISR & I2C_ISR_NACKF_M)
+        {
+            i2c->ICR |= I2C_ICR_STOPCF_M | I2C_ICR_NACKCF_M; // сброс флага STOPF и NACKF
+            xprintf("Ошибка при передаче\n");
             break;
         }
 
-    } 
+        i2c->TXDR = data[i]; // Загрузка передаваемого байта в регистр TXDR
+
+        xprintf("Отправлен байт 0x%02x\n", data[i]);
+        i2c->ICR |= I2C_ICR_STOPCF_M | I2C_ICR_NACKCF_M; // сброс флага STOPF
+    }
+
+
+}
+
+void i2c_slave_read(I2C_TypeDef* i2c, uint8_t data[], uint8_t byte_count)
+{
+
+    xprintf("\nЧтение\n");
+
+    for (uint8_t i = 0; i < byte_count; i++)
+    {
+        // Счетчик для ожидания
+        int counter = 0;
+
+        // Байт нужно прочитать когда RXNE = 1
+        while(!(i2c->ISR & I2C_ISR_RXNE_M))
+        {
+            
+            counter++;
+            if(counter == 1000000)
+            {
+                // Ожидание превышено. Возможно механическое повреждение линии связи
+                break;
+            }
+
+            if((i2c->ISR & I2C_ISR_NACKF_M))
+            {
+                // Мастер прислал NACK. Ошибка чтения
+                break;
+            }
+
+        } 
+
+        // Ожидание превышено. Возможно механическое повреждение линии связи
+        if(counter == 1000000)
+        {
+            xprintf("Разрыв связи\n");
+
+            // Программный сброс модуля i2c и его повторная инициалиизация 
+            i2c_slave_restart(i2c);
+            break;
+        }
+
         if((i2c->ISR & I2C_ISR_NACKF_M))
         {
             xprintf("Ошибка чтения\n");
-            return data;
+            break;
         }
-        // if(i2c->ISR & I2C_ISR_STOPF_M)
-        // {
-        //     i2c->ICR |= I2C_ICR_STOPCF_M | I2C_ICR_NACKCF_M; // сброс флага STOPF и сброс флага NACKF
-        //     xprintf("Сброс. STOPF = %d, NACKF = %d\n", (i2c->ISR & I2C_ISR_STOPF_M) >> I2C_ISR_STOPF_S, (i2c->ISR & I2C_ISR_NACKF_M) >> I2C_ISR_NACKF_S);
-        //     //break;
-        //     //return;
-        // }
-    //xprintf("RXNE = %d\n", (i2c->ISR & I2C_ISR_RXNE_M)>>I2C_ISR_RXNE_S);
-    // for(volatile int i = 0; i < 100; i++)
-    // {
-    //     if (i2c->ISR & I2C_ISR_RXNE_M)
-    //     {
-    //         break;
-    //     }
-    // }
 
-    data = i2c->RXDR; // чтение байта и сброс RXNE
-    xprintf("Чтение %d\n", data);
-    return data;
+        data[i] = i2c->RXDR; // Чтение байта и сброс флага RXNE
+        xprintf("Чтение байта  0x%02x\n", data[i]);
+    }
+
+    xprintf("Прочитано  %d\n", data[0] << 8 | data[1]);
 }
 
 int main()
 {
-    // PM->AHB_CLK_MUX = 0; // выбор источника тактирования. 0 - внешний кварц
-    // PM->CPU_RTC_CLK_MUX = 0;// выбор часового кварца как внешний
-    // for (volatile int i = 0; i < 100; i++); 
-    // WU->CLOCKS_SYS = 1 << 1; // выключение внутреннего системного кварца
-    // WU->CLOCKS_BU = 1 << 1; // выключение внутреннего часовго кварца
-    // for (volatile int i = 0; i < 100; i++) ; 
-    
-    
-    uint8_t slave_adres = 0x36;
-    uint8_t data_byte = 3;
 
-    i2c_init_slave(I2C_0, slave_adres<<1); // инициализация блока i2c в режиме slave 
+    // Адрес ведомого
+    uint8_t slave_address = 0x36;
+    // Число для оптавки
+    uint16_t to_send = 0; 
+    // Массив с байтами для отправки / приема
+    uint8_t data[2];
+
+    // инициализация блока i2c в режиме слейв (ведомый)
+    i2c_slave_init(I2C_0, slave_address); 
     
     while (1)
     {
+        xprintf("\nОжидание\n");
         int counter = 0;
-        xprintf("\nЖду\n");
-        // мастер запросил адрес слейва
+
+        // Ожидание запроса мастером адреса слейва
         while(!(I2C_0->ISR & I2C_ISR_ADDR_M))
         {
             counter++;
             if(counter==10000000)
             {
-                xprintf("\nЖду\n");
+                xprintf("\nОжидание\n");
                 counter = 0;
             }
             
         } 
         
-        if((I2C_0->ISR & I2C_ISR_ADDR_M) == I2C_ISR_ADDR_M)
-        {
-            xprintf("ADDR = 1\n");
-            //xprintf("До сброса ADDR. TXIS = %d, NACKF = %d, STOPF = %d\n", (I2C_0->ISR & I2C_ISR_TXIS_M)>>I2C_ISR_TXIS_S, (I2C_0->ISR & I2C_ISR_NACKF_M)>>I2C_ISR_NACKF_S ,(I2C_0->ISR & I2C_ISR_STOPF_M)>>I2C_ISR_STOPF_S);
-        }
-        I2C_0 -> ISR |=  I2C_ISR_TXE_M;
-        I2C_0->ICR |= I2C_ICR_ADDRCF_M; // срос ADDR
-        //xprintf("Сброс. ADDR = %d\n", (I2C_0->ISR & I2C_ISR_ADDR_M) >> I2C_ISR_ADDR_S);
-        //xprintf("После сброса ADDR. TXIS = %d, NACKF = %d, STOPF = %d\n", (I2C_0->ISR & I2C_ISR_TXIS_M)>>I2C_ISR_TXIS_S, (I2C_0->ISR & I2C_ISR_NACKF_M)>>I2C_ISR_NACKF_S ,(I2C_0->ISR & I2C_ISR_STOPF_M)>>I2C_ISR_STOPF_S);
-        
+        //I2C_0 -> ISR |=  I2C_ISR_TXE_M;
+
+        // срос флага ADDR для подверждения принятия адреса
+        I2C_0->ICR |= I2C_ICR_ADDRCF_M; 
+
         /*
         * I2C_ISR - Регистр прерываний и статуса
         *   
@@ -193,19 +225,32 @@ int main()
         *       1 – передача типа чтения, ведомый переходит в режим передатчика
         * 
         */
+
         if(I2C_0->ISR & I2C_ISR_DIR_M) // DIR = 1. Режим ведомого - передатчик
         {
             xprintf("\nЗапрос на запись\n");
-            i2c_slave_write(I2C_0, data_byte);
+
+            // Отправляется число to_send, состоящее из двух байт
+            // Заполняется массив data байтами, которые нужно отправить
+            data[0] = to_send >> 8;
+            data[1] = to_send & 0b0000000011111111;
+
+            // Отправка
+            i2c_slave_write(I2C_0, data, sizeof(data));
         } 
         else // DIR = 0. Режим ведомого - приемник
         {
             xprintf("\nЗапрос на чтение\n");
-            data_byte = i2c_slave_read(I2C_0);
-            data_byte++; 
-            if(data_byte > 250)
+            
+            // Чтение двух байт от мастера и запись их в массив data 
+            i2c_slave_read(I2C_0, data, sizeof(data));
+
+            // Формирование принятого числа
+            to_send = data[0] << 8 | data[1];
+            to_send++; 
+            if(to_send > 65530)
             {
-                data_byte = 0;
+                to_send = 0;
             }
         }
 
