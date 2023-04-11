d28dfa746896fe7bd6d5927e447c38a9082ad9b8
niffin-lv <matyar000@gmail.com>
2023-04-11 14:51:20 +0300

    HAL_DMA_SPI

---
+++ HAL_DMA_SPI_Slave/src/main.c
@@ -0,0 +1,166 @@
+#include "mik32_hal_rcc.h"
+#include "mik32_hal_spi.h"
+#include "mik32_hal_dma.h"
+
+#include "uart_lib.h"
+#include "xprintf.h"
+
+
+
+SPI_HandleTypeDef hspi0;
+
+DMA_InitTypeDef hdma;
+DMA_ChannelHandleTypeDef hdma_ch0;
+DMA_ChannelHandleTypeDef hdma_ch1;
+
+void SystemClock_Config(void);
+static void SPI0_Init(void);
+static void DMA_CH0_Init(DMA_InitTypeDef* hdma);
+static void DMA_CH1_Init(DMA_InitTypeDef* hdma);
+static void DMA_Init(void);
+
+int main()
+{    
+
+    SystemClock_Config();
+
+    UART_Init(UART_0, 3333, UART_CONTROL1_TE_M | UART_CONTROL1_M_8BIT_M, 0, 0);
+    
+    SPI0_Init();
+
+    DMA_Init();
+
+    uint8_t slave_output[] = {0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7, 0xB8, 0xB9, 0xC0, 0xC1};
+    uint8_t slave_input[sizeof(slave_output)]; 
+    for (uint32_t i = 0; i < sizeof(slave_input); i++)
+    {
+        slave_input[i] = 0;
+    }
+
+    while (1)
+    {    
+
+        HAL_SPI_Enable(&hspi0);
+
+        /* Передача и прием данных */
+        // HAL_SPI_Exchange(&hspi0, master_output, master_input, sizeof(master_output));
+
+        HAL_DMA_Start(&hdma_ch0, slave_output, (void*)&hspi0.Instance->TxData, sizeof(slave_output) - 1);
+        HAL_DMA_Start(&hdma_ch1, (void*)&hspi0.Instance->RxData, slave_input, sizeof(slave_input) - 1);
+        HAL_DMA_Wait(&hdma_ch0); 
+        HAL_DMA_Wait(&hdma_ch1); 
+
+        HAL_SPI_Disable(&hspi0);
+
+
+        //xprintf("Status = 0x%x\n", (uint8_t)hspi0.Instance->IntStatus);
+        /* Вывод принятый данных и обнуление массива master_input */
+        for(uint32_t i = 0; i < sizeof(slave_input); i++)
+        {
+            xprintf("slave_input[%d] = 0x%02x\n", i, slave_input[i]);
+            slave_input[i] = 0;
+        }
+
+        for (volatile int i = 0; i < 1000000; i++);
+    }
+       
+}
+
+void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInit = {0};
+    RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+    RCC_OscInit.OscillatorEnable = RCC_OSCILLATORTYPE_OSC32K | RCC_OSCILLATORTYPE_OSC32M;   
+    RCC_OscInit.OscillatorSystem = RCC_OSCILLATORTYPE_OSC32M;                          
+    RCC_OscInit.AHBDivider = 0;                             
+    RCC_OscInit.APBMDivider = 0;                             
+    RCC_OscInit.APBPDivider = 0;                             
+    RCC_OscInit.HSI32MCalibrationValue = 0;                  
+    RCC_OscInit.LSI32KCalibrationValue = 0;
+    HAL_RCC_OscConfig(&RCC_OscInit);
+
+    PeriphClkInit.PMClockAHB = PMCLOCKAHB_DEFAULT | PM_CLOCK_DMA_M;    
+    PeriphClkInit.PMClockAPB_M = PMCLOCKAPB_M_DEFAULT | PM_CLOCK_WU_M;     
+    PeriphClkInit.PMClockAPB_P = PMCLOCKAPB_P_DEFAULT | PM_CLOCK_UART_0_M | PM_CLOCK_SPI_0_M;
+    PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_NO_CLK;
+    PeriphClkInit.RTCClockCPUSelection = RCC_RTCCLKCPUSOURCE_NO_CLK;
+    HAL_RCC_ClockConfig(&PeriphClkInit);
+}
+
+static void SPI0_Init(void)
+{
+    hspi0.Instance = SPI_0;
+
+    /* Режим SPI */
+    hspi0.Init.SPI_Mode = HAL_SPI_MODE_SLAVE;
+
+    /* Настройки */                       
+    hspi0.Init.CLKPhase = SPI_PHASE_OFF;            
+    hspi0.Init.CLKPolarity = SPI_POLARITY_LOW;         
+    hspi0.Init.DataSize = SPI_DATASIZE_8BITS;  
+
+    HAL_SPI_Init(&hspi0);
+
+}
+
+static void DMA_CH0_Init(DMA_InitTypeDef* hdma)
+{
+    hdma_ch0.dma = hdma;
+    hdma_ch0.ChannelInit.Channel = DMA_CHANNEL_0;  
+    hdma_ch0.ChannelInit.Priority = DMA_CHANNEL_PRIORITY_VERY_HIGH;  
+
+    hdma_ch0.ChannelInit.ReadMode = DMA_CHANNEL_MODE_MEMORY;
+    hdma_ch0.ChannelInit.ReadInc = DMA_CHANNEL_INC_ENABLE;
+    hdma_ch0.ChannelInit.ReadSize = DMA_CHANNEL_SIZE_BYTE;       /* data_len должно быть кратно read_size */
+    hdma_ch0.ChannelInit.ReadBurstSize = 0;                     /* read_burst_size должно быть кратно read_size */
+    hdma_ch0.ChannelInit.ReadRequest = DMA_CHANNEL_SPI_0_REQUEST;
+    hdma_ch0.ChannelInit.ReadAck = DMA_CHANNEL_ACK_DISABLE;
+
+    hdma_ch0.ChannelInit.WriteMode = DMA_CHANNEL_MODE_PERIPHERY;
+    hdma_ch0.ChannelInit.WriteInc = DMA_CHANNEL_INC_DISABLE;
+    hdma_ch0.ChannelInit.WriteSize = DMA_CHANNEL_SIZE_BYTE;     /* data_len должно быть кратно write_size */
+    hdma_ch0.ChannelInit.WriteBurstSize = 0;                    /* write_burst_size должно быть кратно read_size */
+    hdma_ch0.ChannelInit.WriteRequest = DMA_CHANNEL_SPI_0_REQUEST;
+    hdma_ch0.ChannelInit.WriteAck = DMA_CHANNEL_ACK_DISABLE;  
+
+}
+
+static void DMA_CH1_Init(DMA_InitTypeDef* hdma)
+{
+    hdma_ch1.dma = hdma;
+    hdma_ch1.ChannelInit.Channel = DMA_CHANNEL_1;  
+    hdma_ch1.ChannelInit.Priority = DMA_CHANNEL_PRIORITY_VERY_HIGH;  
+
+    hdma_ch1.ChannelInit.ReadMode = DMA_CHANNEL_MODE_PERIPHERY;
+    hdma_ch1.ChannelInit.ReadInc = DMA_CHANNEL_INC_DISABLE;
+    hdma_ch1.ChannelInit.ReadSize = DMA_CHANNEL_SIZE_BYTE;       /* data_len должно быть кратно read_size */
+    hdma_ch1.ChannelInit.ReadBurstSize = 0;                     /* read_burst_size должно быть кратно read_size */
+    hdma_ch1.ChannelInit.ReadRequest = DMA_CHANNEL_SPI_0_REQUEST;
+    hdma_ch1.ChannelInit.ReadAck = DMA_CHANNEL_ACK_DISABLE;
+
+    hdma_ch1.ChannelInit.WriteMode = DMA_CHANNEL_MODE_MEMORY;
+    hdma_ch1.ChannelInit.WriteInc = DMA_CHANNEL_INC_ENABLE;
+    hdma_ch1.ChannelInit.WriteSize = DMA_CHANNEL_SIZE_BYTE;     /* data_len должно быть кратно write_size */
+    hdma_ch1.ChannelInit.WriteBurstSize = 0;                    /* write_burst_size должно быть кратно read_size */
+    hdma_ch1.ChannelInit.WriteRequest = DMA_CHANNEL_SPI_0_REQUEST;  
+    hdma_ch1.ChannelInit.WriteAck = DMA_CHANNEL_ACK_DISABLE;
+
+}
+
+
+static void DMA_Init(void)
+{
+    
+    /* Настройки DMA */
+    hdma.Instance = DMA_CONFIG;
+    hdma.CurrentValue = DMA_CURRENT_VALUE_ENABLE;
+    HAL_DMA_Init(&hdma);
+
+    /* Инициализация канала */
+    DMA_CH0_Init(&hdma);
+    DMA_CH1_Init(&hdma);
+
+}
+
+
