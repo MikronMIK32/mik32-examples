4245b843106871ef01b941b2959f530f6b2bc642
niffin-lv <matyar000@gmail.com>
2023-04-26 11:25:15 +0300

    fix

--- Hal_RTC_IRQ/src/main.c
+++ Hal_RTC_IRQ/src/main.c
@@ -19,7 +19,7 @@ int main()
 {
     SystemClock_Config();
 
-    HAL_EPIC_MaskSet(HAL_EPIC_RTC_MASK);
+    HAL_EPIC_MaskLevelSet(HAL_EPIC_RTC_MASK);
     HAL_IRQ_EnableInterrupts();
     
     UART_Init(UART_0, 3333, UART_CONTROL1_TE_M | UART_CONTROL1_M_8BIT_M, 0, 0);
