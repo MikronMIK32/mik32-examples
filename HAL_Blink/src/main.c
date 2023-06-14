ada2629e67bb35a93af46c1c8ca5ecdc5251fc38
Sergey Shchelkanov <shchelkanov-s@yandex.ru>
2023-06-14 15:10:16 +0300

    update blink example

--- HAL_Blink/src/main.c
+++ HAL_Blink/src/main.c
@@ -1,49 +1,64 @@
 #include "mik32_hal_rcc.h"
+#include "mik32_hal_pad_config.h"
 #include "mik32_hal_gpio.h"
 
 #include "uart_lib.h"
 #include "xprintf.h"
 
-/* Тип платы */
-#define BOARD_LITE_V0
+#include <limits.h>
 
-#ifdef BOARD_LITE_V0
+/* Тип платы */
+#define BOARD_DIP
+
+#ifdef BOARD_LITE
 #define PIN_LED 	PORT2_7
 #define PIN_BUTTON 	PORT2_6
 #endif 
 
+#ifdef BOARD_DIP
+#define PIN_LED0 	PORT0_3
+#define PIN_LED1 	PORT1_3
+#endif 
 
-void SystemClock_Config(void);
+
+void SystemClock_Config();
+void GPIO_Config();
+
+
+#define BLINK_LOOP_ITERS 1000000
 
 void ledBlink() 
 {
-	#ifdef BOARD_LITE_V0
+	#ifdef BOARD_LITE
 
-    HAL_GPIO_PinWrite(PIN_LED, PIN_HIGH);
+    HAL_GPIO_PinWrite(PIN_LED, GPIO_PIN_HIGH);
 	xprintf("ON \n");
-	for (volatile int i = 0; i < 100000; i++);
+	for (volatile int i = 0; i < BLINK_LOOP_ITERS; i++);
 
-    HAL_GPIO_PinWrite(PIN_LED, PIN_LOW);  
+    HAL_GPIO_PinWrite(PIN_LED, GPIO_PIN_LOW);  
 	xprintf("OFF \n");
-	for (volatile int i = 0; i < 100000; i++);
+	for (volatile int i = 0; i < BLINK_LOOP_ITERS; i++);
 
 	#endif 
+
+    #ifdef BOARD_DIP
+    HAL_GPIO_PinToggle(PIN_LED0);
+    HAL_GPIO_PinToggle(PIN_LED1);
+    for (volatile int i = 0; i < BLINK_LOOP_ITERS; i++);
+    #endif
 }
 
 void ledButton() 
 {
-	#ifdef BOARD_LITE_V0
-
+	#ifdef BOARD_LITE
 	if(HAL_GPIO_PinRead(PIN_BUTTON))
 	{
-        HAL_GPIO_PinWrite(PIN_LED, PIN_HIGH);
+        HAL_GPIO_PinWrite(PIN_LED, GPIO_PIN_HIGH);
+        return;
 	}
-	else
-	{
-        HAL_GPIO_PinWrite(PIN_LED, PIN_LOW);  
-	}
-
 	#endif 
+
+    ledBlink();
 }
 
 
@@ -51,24 +66,17 @@ int main()
 {    
 
     SystemClock_Config();
+    GPIO_Config();
 
 	UART_Init(UART_0, 3333, UART_CONTROL1_TE_M | UART_CONTROL1_M_8BIT_M, 0, 0);
 
-	#ifdef BOARD_LITE_V0
-
-    HAL_GPIO_PinOutputInit(PIN_LED, PIN_LOW);
-    HAL_GPIO_PinInputInit(PIN_BUTTON, PIN_PULL_NONE);
-
-	#endif 
-
 	while (1) {
-		ledBlink(); /* Светодиод мигает */
-		// ledButton(); /* Светодиод зажигается при нажатой кнопке */ 
+		ledButton();
 	}
        
 }
 
-void SystemClock_Config(void)
+void SystemClock_Config()
 {
     RCC_OscInitTypeDef RCC_OscInit = {0};
     RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
@@ -88,4 +96,27 @@ void SystemClock_Config(void)
     PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_NO_CLK;
     PeriphClkInit.RTCClockCPUSelection = RCC_RTCCLKCPUSOURCE_NO_CLK;
     HAL_RCC_ClockConfig(&PeriphClkInit);
+}
+
+void GPIO_Config()
+{
+    #ifdef BOARD_LITE
+    HAL_PadConfig_PinMode(PIN_LED, PIN_MODE1);
+    HAL_PadConfig_PinMode(PIN_BUTTON, PIN_MODE1);
+    HAL_PadConfig_PinPull(PIN_BUTTON, PIN_PULL_DOWN);
+
+    HAL_GPIO_PinDirection(PIN_LED, GPIO_PIN_OUTPUT);
+    HAL_GPIO_PinDirection(PIN_BUTTON, GPIO_PIN_INPUT);
+    HAL_GPIO_PinWrite(PIN_LED, GPIO_PIN_LOW);
+    #endif
+
+    #ifdef BOARD_DIP
+    HAL_PadConfig_PinMode(PIN_LED0, PIN_MODE1);
+    HAL_PadConfig_PinMode(PIN_LED1, PIN_MODE1);
+
+    HAL_GPIO_PinDirection(PIN_LED0, GPIO_PIN_OUTPUT);
+    HAL_GPIO_PinDirection(PIN_LED1, GPIO_PIN_OUTPUT);
+    HAL_GPIO_PinWrite(PIN_LED0, GPIO_PIN_LOW);
+    HAL_GPIO_PinWrite(PIN_LED1, GPIO_PIN_LOW);
+    #endif
 }
