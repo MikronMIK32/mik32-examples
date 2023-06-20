88401ea8401b7f2c84cefec52948f47e82e439fa
niffin-lv <matyar000@gmail.com>
2023-06-20 13:33:02 +0300

    Обновление HAL_I2C

--- Hal_I2C_Master/src/main.c
+++ Hal_I2C_Master/src/main.c
@@ -1,5 +1,7 @@
 #include "mik32_hal_rcc.h"
+#include "mik32_hal_pad_config.h"
 #include "mik32_hal_i2c.h"
+
 
 #include "uart_lib.h"
 #include "xprintf.h"
@@ -15,42 +17,63 @@ int main()
 
     SystemClock_Config();
 
+    HAL_PadConfig_PinMode(PORT0_5 | PORT0_6, PIN_MODE0);    /* Режим работы выводов UART0 */
+    HAL_PadConfig_PinMode(PORT0_9 | PORT0_10, PIN_MODE0);   /* Режим работы выводов I2C */
+
     UART_Init(UART_0, 3333, UART_CONTROL1_TE_M | UART_CONTROL1_M_8BIT_M, 0, 0);
-    
     I2C0_Init();
 
-    // Адрес ведомого
-    uint16_t slave_address = 0x2BB; //0x36 0x3FF 0x7F
-
-    uint8_t data[I2C_DATA_BYTES];
+    uint8_t data[10];
 
     for(int i = 0; i < sizeof(data); i++)
     {
         data[i] = (uint8_t)i;
-        xprintf("data[%d] = %d\n", i, data[i]);
     }
- 
+
+    HAL_StatusTypeDef error_code;
     while (1)
     {    
-
-        /*Запись данных по адресу slave_address = 0x36 без сдвига адреса*/
-        HAL_I2C_Master_WriteNBYTE(&hi2c0, 0x36, data, sizeof(data)); 
-        if(hi2c0.Init.AutoEnd == AUTOEND_DISABLE)
+        for(int i = 0; i < sizeof(data); i++)
         {
-            /*Формирование события STOP*/
-            HAL_I2C_Master_Stop(&hi2c0);
+            data[i] = (uint8_t)i;
         }
-        for (volatile int i = 0; i < 100000; i++); 
-
-
-        /*Чтение данных по адресу slave_address = 0x36 без сдвига адреса*/
-        HAL_I2C_Master_Read(&hi2c0, slave_address, data, sizeof(data)); 
-        if(hi2c0.Init.AutoEnd == AUTOEND_DISABLE)
+        /* Запись данных по адресу 0x36 */
+        xprintf("\nMaster_write\n");
+        error_code = HAL_I2C_Master_Transmit(&hi2c0, 0x36, data, sizeof(data), I2C_TIMEOUT_DEFAULT);
+        if (error_code != HAL_OK)
         {
-            /*Формирование события STOP*/
-            HAL_I2C_Master_Stop(&hi2c0);
+            xprintf("Master_Transmit error #%d, code %d, ISR %d\n", error_code, hi2c0.ErrorCode, hi2c0.Instance->ISR);
+            HAL_I2C_Reset(&hi2c0);
         }
-        for (volatile int i = 0; i < 1000000; i++);
+        if (hi2c0.Init.AutoEnd == I2C_AUTOEND_DISABLE)
+        {
+            hi2c0.Instance->CR2 |= I2C_CR2_STOP_M;
+        }
+        for (volatile int i = 0; i < 1000000; i++); 
+
+
+
+        /* Чтение данных по адресу 0x36 */
+        xprintf("\nMaster_read\n");
+        error_code = HAL_I2C_Master_Receive(&hi2c0, 0x36, data, sizeof(data), I2C_TIMEOUT_DEFAULT);
+        if (error_code != HAL_OK)
+        {
+            xprintf("Master_Receive error #%d, code %d, ISR %d\n", error_code, hi2c0.ErrorCode, hi2c0.Instance->ISR);
+            HAL_I2C_Reset(&hi2c0);
+        }
+        else
+        {
+            for(int i = 0; i < sizeof(data); i++)
+            {
+                xprintf("Data_read[%d] = %d\n", i, data[i]);
+                data[i] = 0;
+            }
+        }
+        if (hi2c0.Init.AutoEnd == I2C_AUTOEND_DISABLE)
+        {
+            hi2c0.Instance->CR2 |= I2C_CR2_STOP_M;
+        }
+        for (volatile int i = 0; i < 1000000; i++); 
 
     }
        
@@ -81,24 +104,26 @@ void SystemClock_Config(void)
 static void I2C0_Init(void)
 {
 
-    /*Общие настройки*/
+    /* Общие настройки */
     hi2c0.Instance = I2C_0;
-    hi2c0.Mode = HAL_I2C_MODE_MASTER;
-    hi2c0.ShiftAddress = SHIFT_ADDRESS_DISABLE;
-    hi2c0.Init.AddressingMode = I2C_ADDRESSINGMODE_10BIT;
-    hi2c0.Init.DualAddressMode = I2C_DUALADDRESS_ENABLE; // При ENABLE в режиме ведущего значение AddressingMode не влияет
-    hi2c0.Init.Filter = I2C_FILTER_OFF;
 
-    /*Настройка частоты*/
+    hi2c0.Init.Mode = HAL_I2C_MODE_MASTER;
+
+    hi2c0.Init.DigitalFilter = I2C_DIGITALFILTER_OFF;
+    hi2c0.Init.AnalogFilter = I2C_ANALOGFILTER_DISABLE;
+    hi2c0.Init.AutoEnd = I2C_AUTOEND_ENABLE;
+
+    /* Настройка частоты */
     hi2c0.Clock.PRESC = 5;
     hi2c0.Clock.SCLDEL = 10;
-    hi2c0.Clock.SDADEL = 10;
-    hi2c0.Clock.SCLH = 16;
-    hi2c0.Clock.SCLL = 16;
+    hi2c0.Clock.SDADEL = 15;
+    hi2c0.Clock.SCLH = 15;
+    hi2c0.Clock.SCLL = 15;
 
-    /*Настройки ведущего*/
-    hi2c0.Init.AutoEnd = AUTOEND_ENABLE;
 
-    HAL_I2C_Init(&hi2c0);
+    if (HAL_I2C_Init(&hi2c0) != HAL_OK)
+    {
+        xprintf("I2C_Init error\n");
+    }
 
 }
--- Hal_I2C_Slave/src/main.c
+++ Hal_I2C_Slave/src/main.c
@@ -1,41 +1,74 @@
 #include "mik32_hal_rcc.h"
+#include "mik32_hal_pad_config.h"
 #include "mik32_hal_i2c.h"
+
 
 #include "uart_lib.h"
 #include "xprintf.h"
 
+
 I2C_HandleTypeDef hi2c0;
-uint8_t flag = 0;
 
 void SystemClock_Config(void);
 static void I2C0_Init(void);
 
 int main()
-{
-   
+{    
+
     SystemClock_Config();
 
-    UART_Init(UART_0, 3333, UART_CONTROL1_TE_M | UART_CONTROL1_M_8BIT_M, 0, 0);
+    HAL_PadConfig_PinMode(PORT0_5 | PORT0_6, PIN_MODE0);    /* Режим работы выводов UART0 */
+    HAL_PadConfig_PinMode(PORT0_9 | PORT0_10, PIN_MODE0);   /* Режим работы выводов I2C */
 
+    UART_Init(UART_0, 3333, UART_CONTROL1_TE_M | UART_CONTROL1_M_8BIT_M, 0, 0);
     I2C0_Init();
-    
-    // Массив с байтами для отправки / приема
-    uint8_t data[I2C_DATA_BYTES];
+
+
+    uint8_t data[10];
 
     for(int i = 0; i < sizeof(data); i++)
     {
-        data[i] = (uint8_t)i; 
+        data[i] = (uint8_t)i;
     }
 
+    HAL_StatusTypeDef error_code;
     while (1)
-    {
-        /*Ведущий отправляет - ведомый принимает*/
-        HAL_I2C_Slave_Read(&hi2c0, data, sizeof(data));
-        
-        /*Ведущий принимает - ведомый отправляет*/
-        HAL_I2C_Slave_Write(&hi2c0, data, sizeof(data));
+    {    
+
+        /* Чтение данных */
+        xprintf("\nSlave_read\n");
+        error_code = HAL_I2C_Slave_Receive(&hi2c0, data, sizeof(data), 5*I2C_TIMEOUT_DEFAULT);
+        if (error_code != HAL_OK)
+        {
+            xprintf("Slave_Receive error #%d, code %d, ISR %d\n", error_code, hi2c0.ErrorCode, hi2c0.Instance->ISR);
+            HAL_I2C_Reset(&hi2c0);
+        }
+        else
+        {
+            for(int i = 0; i < sizeof(data); i++)
+            {
+                xprintf("Data_read[%d] = %d\n", i, data[i]);
+                data[i] = 0;
+            }
+        }
+
+
+
+        for(int i = 0; i < sizeof(data); i++)
+        {
+            data[i] = (uint8_t)i;
+        }
+        /* Запись данных */
+        xprintf("\nSlave_write\n");
+        error_code = HAL_I2C_Slave_Transmit(&hi2c0, data, sizeof(data), 5*I2C_TIMEOUT_DEFAULT);
+        if (error_code != HAL_OK)
+        {
+            xprintf("Slave_Transmit error #%d, code %d, ISR %d\n", error_code, hi2c0.ErrorCode, hi2c0.Instance->ISR);
+            HAL_I2C_Reset(&hi2c0);
+        }
+
     }
-    
+       
 }
 
 void SystemClock_Config(void)
@@ -65,40 +98,46 @@ static void I2C0_Init(void)
 
     /*Общие настройки*/
     hi2c0.Instance = I2C_0;
-    hi2c0.Mode = HAL_I2C_MODE_SLAVE;
-    hi2c0.ShiftAddress = SHIFT_ADDRESS_ENABLE; 
-    hi2c0.Init.AddressingMode = I2C_ADDRESSINGMODE_10BIT;
-    hi2c0.Init.DualAddressMode = I2C_DUALADDRESS_ENABLE; // При ENABLE в режиме ведущего значение AddressingMode не влияет
-    hi2c0.Init.Filter = I2C_FILTER_OFF;
 
-    /*Настройка частоты*/
+    hi2c0.Init.Mode = HAL_I2C_MODE_SLAVE;
+    
+    hi2c0.Init.DigitalFilter = I2C_DIGITALFILTER_OFF;
+    hi2c0.Init.AnalogFilter = I2C_ANALOGFILTER_DISABLE;
+    hi2c0.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+
+    hi2c0.Init.OwnAddress1 = 0x36;
+    hi2c0.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+    hi2c0.Init.OwnAddress2Mask = I2C_OWNADDRESS2_MASK_DISABLE;
+    hi2c0.Init.OwnAddress2 = 0x55;
+    hi2c0.Init.GeneralCall = I2C_GENERALCALL_DISABLE;
+    hi2c0.Init.SBCMode = I2C_SBC_DISABLE; 
+
+
+    /* Настройка частоты */
     hi2c0.Clock.PRESC = 5;
     hi2c0.Clock.SCLDEL = 10;
-    hi2c0.Clock.SDADEL = 10;
-    hi2c0.Clock.SCLH = 16;
-    hi2c0.Clock.SCLL = 16;
+    hi2c0.Clock.SDADEL = 15;
+    hi2c0.Clock.SCLH = 15;
+    hi2c0.Clock.SCLL = 15;
 
-    /*Настройки ведомого*/
-    hi2c0.Init.OwnAddress1 = 0x2BB; //0x36 0x3FF
-    hi2c0.Init.OwnAddress2 = 0x36; //0x57
-    hi2c0.Init.OwnAddress2Mask = I2C_OWNADDRESS2_MASK_DISABLE;
-    hi2c0.Init.SBCMode = I2C_SBC_DISABLE;
-    hi2c0.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE; // Не совместим с режимом SBC
 
-    HAL_I2C_Init(&hi2c0);
+    if (HAL_I2C_Init(&hi2c0) != HAL_OK)
+    {
+        xprintf("I2C_Init error\n");
+    }
 
 }
 
-void HAL_I2C_Slave_SBC(I2C_HandleTypeDef *hi2c, uint32_t byte_count)
+HAL_StatusTypeDef HAL_I2C_Slave_SBC(I2C_HandleTypeDef *hi2c, uint8_t *pData, uint16_t DataSize, uint32_t ByteCount)
 {
-
-    if(byte_count == 2)
+    if (ByteCount == 5)
     {
         HAL_I2C_Slave_NACK(hi2c);
-    }
-    else
-    {
-        HAL_I2C_Slave_ACK(hi2c);
+        return HAL_ERROR;
     }
     
+    
+    HAL_I2C_Slave_ACK(hi2c);
+
+    return HAL_OK;
 }
