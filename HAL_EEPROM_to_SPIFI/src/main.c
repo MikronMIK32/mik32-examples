5eff0f6736562821335122d21f5cd8e8c7e487e9
spam-receiver <48826370+spam-receiver@users.noreply.github.com>
2024-07-19 00:55:44 +0300

    Добавлен XIP режим
    В режиме QPI отключены команды путём регулярной передачи 0x20 в промежуточном байте.

--- HAL_EEPROM_to_SPIFI/src/main.c
+++ HAL_EEPROM_to_SPIFI/src/main.c
@@ -4,50 +4,63 @@
 #include "riscv_csr_encoding.h"
 #include "csr.h"
 
-/*
- * Пример начального загрузчика для использования SPIFI в связке с внешней
- * микросхемой флеш-памяти в роли памяти программ, адресуемой МК автоматически.
+#include "uart_lib.h"
+#include "xprintf.h"
+
+/* 
+ *        !!! Полный текст описания читайте ниже кода программы !!!
+ *
+ * Пример начального загрузчика для использования SPIFI в связке с внешней МФП
+ * (Микросхемой Флеш-Памяти) в роли памяти программ, адресуемой автоматически.
  * В примере запускается тактирование SPIFI и настраиваются выводы, включается
- * кэширование и устанавливается адрес входа в прерывание. После чего выполняется
- * команда перехода программы в начало адресного пространства внешней флеш-памяти.
+ * кэширование и устанавливается адрес входа в прерывание. После этого следует
+ * инструкция выборки команд из начального адресного пространства внешней МФП.
  *  
- * Варианты настройки константы "CHIP_MODE":
+ *  Варианты настройки константы "CHIP_MODE":
  * 
- *  0 = Автовыбор по итогу проверки связи: [SPI]->[QSPI_DO]->[QSPI_I/O]->[QPI].
+ *  0 = Автовыбор по итогу проверки связи: [SPI]->[QDO]->[QIO]->[QPI]->[XIP]
  *  1 = SPI — Весь обмен по одной линии связи. Это самый медленный вариант.
- *  2 = Quad Data Output — По четырём линиям связи передаются только данные.
- *  3 = Quad Input/Output — По четырём линиям не передаются только команды.
- *  4 = QPI — Весь обмен всегда происходит только в четырёхпроводном режиме.
+ *  2 = QDO — По четырём линиям связи передаются только данные.
+ *  3 = QIO — По четырём линиям не передаются только команды.
+ *  4 = QPI — Весь обмен осуществляется в четырёхпроводном режиме.
+ *  5 = XIP — Весь обмен четырёхпроводной, но вообще без передачи команд.
  * 
- * Любое значение вне диапазона чисел от 0 до 4 будет восприниматься как 0.
+ *  Любое значение вне диапазона чисел от 0 до 5 будет восприниматься как 0.
  * 
- * Рекомендуется использовать режим QPI, при котором задержка выборки данных из
- * внешней МФП минимальна, что особенно важно при выполнении из неё программ по
- * технологии XIP (Execute-In-Place — выполнение на месте).
- * 
- *      !!! Полный текст описания читайте ниже кода программы !!!
  */
 
-#define CHIP_MODE 0            /* Выбор протоколов связи c МФП (варианты: 0 ~ 4) */
+#define CHIP_MODE 0            /* Выбор протоколов связи c МФП (варианты: 0 ~ 5) */
 #define CHIP_SIZE 8*1024*1024  /* Объём внешней флеш-памяти (от 128 кБ до 16 МБ) */
 #define TEST_ADDR 0            /* Начальный адрес проверки (по всему объёму МФП) */
 #define TEST_SIZE 256          /* Размер тестируемого участка (от 1 до 256 байт) */
+
+#define XPRINTF_UART UART_1
+#define XPRINTF_UART_SPEED 9600
+#define XPRINTF_UART_DEBUG 0
 
 void SystemClock_Config();
 void SPIFI_Init();
 
 int main() {
-    
+
     HAL_Init();
 
     SystemClock_Config();
-    
+
+    UART_Init(XPRINTF_UART, (32000000/XPRINTF_UART_SPEED), UART_CONTROL1_TE_M | UART_CONTROL1_M_8BIT_M, 0, 0, XPRINTF_UART_DEBUG);
+
+    xprintf_uarts_allow(XPRINTF_UART_1);
+
+    xprintf("\nEEPROM->SPIFI Bootloader Started with ");
+
     SPIFI_Init();
-    
+
     write_csr(mtvec, 0x80000000);
-    
+
     asm volatile("la ra, 0x80000000\n\t"
                  "jalr ra");
+    
+    xprintf("Error 3\n\n");
     while (1);
 }
 
@@ -69,7 +82,7 @@ void SystemClock_Config(void) {
 
 #if CHIP_MODE < 0
 #define CHIP_MODE 0
-#elif CHIP_MODE > 4
+#elif CHIP_MODE > 5
 #define CHIP_MODE 0
 #endif
 
@@ -91,8 +104,16 @@ void SystemClock_Config(void) {
 #define TEST_ADDR 0
 #endif
 
+#define AUTO_MODE 0
+#define SPI_MODE 1
+#define QDO_MODE 2
+#define QIO_MODE 3
+#define QPI_MODE 4
+#define XIP_MODE 5
+
 uint8_t spi_bytes[TEST_SIZE] = {};
 uint8_t tmp_bytes[TEST_SIZE] = {};
+uint8_t chip_mode = 0;
 
 bool cmp_bytes(uint8_t *bytes_a, uint8_t *bytes_b, uint16_t size) {
     for (uint16_t i = 0; i < size; i++) {
@@ -114,7 +135,7 @@ void SPIFI_Init() {
     HAL_SPIFI_MspInit(&spifi);
     HAL_SPIFI_Reset(&spifi);
 
-    HAL_SPIFI_W25_QPIDisable(&spifi);
+    HAL_SPIFI_W25_Reset_QPI(&spifi);
 
     clean_bytes(spi_bytes);
     HAL_SPIFI_W25_ReadData(&spifi, TEST_ADDR, TEST_SIZE, spi_bytes);
@@ -130,82 +151,123 @@ void SPIFI_Init() {
     }
     
     if (count_00 < 1 || count_ff < 1) {
+        xprintf("Error 0\n\n");
         while (1);
     }
 
     clean_bytes(tmp_bytes);
     HAL_SPIFI_W25_ReadData(&spifi, TEST_ADDR, TEST_SIZE, tmp_bytes);
-    if (cmp_bytes(tmp_bytes, spi_bytes, TEST_SIZE) == false) {
-         while (1); 
+    if (cmp_bytes(tmp_bytes, spi_bytes, TEST_SIZE) == 0) {
+        xprintf("Error 1\n\n");
+        while (1); 
     }
-
+    chip_mode = SPI_MODE;
     SPIFI_MemoryCommandTypeDef cmd_mem = {
         .FieldForm = SPIFI_CONFIG_CMD_FIELDFORM_ALL_SERIAL,
-        .FrameForm = SPIFI_CONFIG_CMD_FRAMEFORM_OPCODE_3ADDR
+        .FrameForm = SPIFI_CONFIG_CMD_FRAMEFORM_OPCODE_3ADDR,
+        .InterimData = 0
     };
-#if CHIP_MODE != 1
-    uint8_t sreg2 = HAL_SPIFI_W25_ReadSREG(&spifi, W25_SREG2);
-    if ((sreg2 & (1 << 1)) == false) {
-        uint8_t sreg1 = HAL_SPIFI_W25_ReadSREG(&spifi, W25_SREG1);
-        if (HAL_SPIFI_W25_WriteSREG(&spifi, sreg1, sreg2 | (1 << 1)) != 0) {
-            HAL_SPIFI_W25_WriteDisable(&spifi);
-        }
-    }
-
-    sreg2 = HAL_SPIFI_W25_ReadSREG(&spifi, W25_SREG2);
-    if ((sreg2 & (1 << 1)) != false) {
-#if CHIP_MODE == 0 | CHIP_MODE == 2
+#if CHIP_MODE != SPI_MODE
+    if (HAL_SPIFI_W25_QuadEnable(&spifi) == HAL_OK) {
+#if CHIP_MODE == AUTO_MODE | CHIP_MODE == QDO_MODE
         clean_bytes(tmp_bytes);
         HAL_SPIFI_W25_ReadData_Quad(&spifi, TEST_ADDR, TEST_SIZE, tmp_bytes);
         if (cmp_bytes(tmp_bytes, spi_bytes, TEST_SIZE) == 1) {
-            cmd_mem.FieldForm = SPIFI_CONFIG_CMD_FIELDFORM_DATA_PARALLEL;
+            chip_mode = QDO_MODE;
         }
 #endif
-#if CHIP_MODE == 0 | CHIP_MODE == 3
+#if CHIP_MODE == AUTO_MODE | CHIP_MODE == QIO_MODE
         clean_bytes(tmp_bytes);
         HAL_SPIFI_W25_ReadData_Quad_IO(&spifi, TEST_ADDR, TEST_SIZE, tmp_bytes);
         if (cmp_bytes(tmp_bytes, spi_bytes, TEST_SIZE) == 1) {
-            cmd_mem.FieldForm = SPIFI_CONFIG_CMD_FIELDFORM_OPCODE_SERIAL;
+            chip_mode = QIO_MODE;
         }
 #endif
-#if CHIP_MODE == 0 | CHIP_MODE == 4
+#if CHIP_MODE == AUTO_MODE | CHIP_MODE == QPI_MODE
         clean_bytes(tmp_bytes);
         HAL_SPIFI_W25_QPIEnable(&spifi);
         HAL_SPIFI_W25_ReadData_Quad_IO_QPI(&spifi, TEST_ADDR, TEST_SIZE, tmp_bytes);
         if (cmp_bytes(tmp_bytes, spi_bytes, TEST_SIZE) == 1) {
-            cmd_mem.FieldForm = SPIFI_CONFIG_CMD_FIELDFORM_ALL_PARALLEL;
+            chip_mode = QPI_MODE;
         } else {
             HAL_SPIFI_W25_QPIDisable(&spifi);
         }
 #endif
+#if CHIP_MODE == AUTO_MODE
+        if (chip_mode == QPI_MODE) {
+            clean_bytes(tmp_bytes);
+            HAL_SPIFI_W25_ReadData_Quad_IO_QPI_XIP_Init(&spifi, TEST_ADDR, TEST_SIZE, tmp_bytes);
+            if (cmp_bytes(tmp_bytes, spi_bytes, TEST_SIZE) == 1) {
+                clean_bytes(tmp_bytes);
+                HAL_SPIFI_W25_ReadData_Quad_IO_QPI_XIP(&spifi, TEST_ADDR, TEST_SIZE, tmp_bytes);
+                if (cmp_bytes(tmp_bytes, spi_bytes, TEST_SIZE) == 1) {
+                    chip_mode = XIP_MODE;
+                }
+            }
+        }
+#endif
+#if CHIP_MODE == XIP_MODE
+        clean_bytes(tmp_bytes);
+        HAL_SPIFI_W25_QPIEnable(&spifi);
+        HAL_SPIFI_W25_ReadData_Quad_IO_QPI_XIP_Init(&spifi, TEST_ADDR, TEST_SIZE, tmp_bytes);
+        if (cmp_bytes(tmp_bytes, spi_bytes, TEST_SIZE) == 1) {
+            clean_bytes(tmp_bytes);
+            HAL_SPIFI_W25_ReadData_Quad_IO_QPI_XIP(&spifi, TEST_ADDR, TEST_SIZE, tmp_bytes);
+            if (cmp_bytes(tmp_bytes, spi_bytes, TEST_SIZE) == 1) {
+                chip_mode = XIP_MODE;
+            } else {
+                HAL_SPIFI_W25_Reset_QPI(&spifi);
+            }
+        } else {
+            HAL_SPIFI_W25_Reset_QPI(&spifi);
+        }
+#endif
     }
 #endif
-    switch (cmd_mem.FieldForm){
-    case (SPIFI_CONFIG_CMD_FIELDFORM_ALL_SERIAL):
+    switch (chip_mode) {
+    case (SPI_MODE):
         cmd_mem.InterimLength = 0;
         cmd_mem.OpCode = 0x03;
+        xprintf("SPI Mode\n\n");
         break;
-#if CHIP_MODE != 1
-#if CHIP_MODE == 0 | CHIP_MODE == 2
-    case (SPIFI_CONFIG_CMD_FIELDFORM_DATA_PARALLEL):
+#if CHIP_MODE != SPI_MODE
+#if CHIP_MODE == AUTO_MODE | CHIP_MODE == QDO_MODE
+    case (QDO_MODE):
+        cmd_mem.FieldForm = SPIFI_CONFIG_CMD_FIELDFORM_DATA_PARALLEL;
         cmd_mem.InterimLength = 1;
         cmd_mem.OpCode = 0x6B;
+        xprintf("QDO Mode\n\n");
         break;
 #endif
-#if CHIP_MODE == 0 | CHIP_MODE == 3
-    case (SPIFI_CONFIG_CMD_FIELDFORM_OPCODE_SERIAL):
+#if CHIP_MODE == AUTO_MODE | CHIP_MODE == QIO_MODE
+    case (QIO_MODE):
+        cmd_mem.FieldForm = SPIFI_CONFIG_CMD_FIELDFORM_OPCODE_SERIAL;
         cmd_mem.InterimLength = 3;
         cmd_mem.OpCode = 0xEB;
+        xprintf("QIO Mode\n\n");
         break;
 #endif
-#if CHIP_MODE == 0 | CHIP_MODE == 4
-    case (SPIFI_CONFIG_CMD_FIELDFORM_ALL_PARALLEL):
+#if CHIP_MODE == AUTO_MODE | CHIP_MODE == QPI_MODE
+    case (QPI_MODE):
+        cmd_mem.FieldForm = SPIFI_CONFIG_CMD_FIELDFORM_ALL_PARALLEL;
         cmd_mem.InterimLength = 1;
         cmd_mem.OpCode = 0xEB;
+        xprintf("QPI Mode\n\n");
+        break;
+#endif
+#if CHIP_MODE == AUTO_MODE | CHIP_MODE == XIP_MODE
+    case (XIP_MODE):
+        cmd_mem.FieldForm = SPIFI_CONFIG_CMD_FIELDFORM_ALL_PARALLEL;
+        cmd_mem.FrameForm = SPIFI_CONFIG_CMD_FRAMEFORM_NOOPCODE_3ADDR;
+        cmd_mem.InterimLength = 1;
+        cmd_mem.InterimData = 0x20;
+        cmd_mem.OpCode = 0xEB;
+        xprintf("XIP Mode\n\n");
         break;
 #endif
 #endif
     default:
+        xprintf("Error 2\n\n");
         while (1);
     }
 
@@ -213,7 +275,7 @@ void SPIFI_Init() {
         .Instance = spifi.Instance,
         .CacheEnable = SPIFI_CACHE_ENABLE,
         .CacheLimit = 0x90000000,
-        .Command = cmd_mem,
+        .Command = cmd_mem
     };
 
     HAL_SPIFI_MemoryMode_Init(&spifi_mem);
@@ -223,8 +285,8 @@ void SPIFI_Init() {
  * Данный пример демонстрирует настройку интерфейса SPIFI в связке с внешней
  * микросхемой флеш-памяти (далее МФП — сокращение от Микросхема Флеш-Памяти).
  * 
- * Пример начального загрузчика для использования SPIFI в связке с внешней
- * микросхемой флеш-памяти в роли памяти программ, адресуемой МК автоматически.
+ * Пример начального загрузчика для использования SPIFI в связке с внешней МФП
+ * в роли памяти программ и данных, адресуемой микроконтроллером автоматически.
  * В примере запускается тактирование SPIFI и настраиваются выводы, включается
  * кэширование и устанавливается адрес входа в прерывание. После чего выполняется
  * команда перехода программы в начало адресного пространства внешней флеш-памяти.
@@ -260,26 +322,42 @@ void SPIFI_Init() {
  * всяких проблем и изменений кода данной программы подойдут аналоги таких
  * серий как: W25Q16JV-DTR, W25Q32FV, W25Q64FV, W25Q128FV, W25Q256FV, а также
  * W25Q512JV-DTR. Все они поддерживают режим QPI, т.е. максимум быстродействия
- * интерфейса SPIFI для MIK32 Амур. Небольшие изменения данной программы дадут
+ * интерфейса SPIFI для MIK32 Амур. Небольшие изменения данного примера дадут
  * возможность использовать адресное пространство больше 16 МБ, режим адресации
  * которого всего 24 бита. Переключение в режим 32 бита расширяет программную
  * память до 2 ГБ из МФП с интерфейсами типа DUAL/QUAD SPI и QPI, работающих
  * при напряжениях питания и сигналов в 3,3 В.
+ * 
+ * Также у всех вышеперечисленных микросхем есть режим Performance Enhance Mode
+ * - XIP (execute-in-place), который позволяет при каждой последующей операции
+ * не отправлять повторно ту же самую команду. Это экономит 8 тактов команды в
+ * QSPI режиме и 2 такта команды в QPI режиме. W25Q64FV работает без проблем,
+ * у остальных должно работать точно также, судя по документации. Для этого
+ * нужно выполнить команду чтения любого адреса, где в промежуточных данных
+ * после него нужно отправить 0x20. Затем переключаем SPIFI в режим без кода
+ * операции, где только 3 байта адреса в запросе плюс промежуточный байт данных
+ * 0x20. Для других типов микросхем, например EN25Q80B, этот байт должен иметь
+ * исключающие друг друга полубайты, например: 0xA5, 0x5A, 0x0F, 0xF0 и т.п.
+ * У самых дешёвых микросхем время ожидания вывода данных даже на относительно
+ * низких тактовых частотах может составлять от четырёх до шести тактов (2—3
+ * байта), а иногда и больше.
  *  
- * Варианты настройки константы "CHIP_MODE":
+ *  Варианты настройки константы "CHIP_MODE":
  * 
- *  0 = Автовыбор по итогу проверки связи: [SPI]->[QSPI_DO]->[QSPI_I/O]->[QPI].
+ *  0 = Автовыбор по итогу проверки связи: [SPI]->[QDO]->[QIO]->[QPI]->[XIP]
  *  1 = SPI — Весь обмен по одной линии связи. Это самый медленный вариант.
- *  2 = Quad Data Output — По четырём линиям связи передаются только данные.
- *  3 = Quad Input/Output — По четырём линиям не передаются только команды.
- *  4 = QPI — Весь обмен всегда происходит только в четырёхпроводном режиме.
+ *  2 = QDO — По четырём линиям связи передаются только данные.
+ *  3 = QIO — По четырём линиям не передаются только команды.
+ *  4 = QPI — Весь обмен осуществляется в четырёхпроводном режиме.
+ *  5 = XIP — Весь обмен четырёхпроводной, но вообще без передачи команд.
  * 
- * Любое значение вне диапазона чисел от 0 до 4 будет восприниматься как 0.
+ *  Любое значение вне диапазона чисел от 0 до 5 будет восприниматься как 0.
  * 
  * В зависимости от выбора в программе подключаются/исключаются те или иные части
  * кода, что естественно изменяет размер получаемой из него прошивки.
  * 
- * Рекомендуется использовать режим QPI, при котором задержка выборки данных из
- * внешней МФП минимальна, что особенно важно при выполнении из неё программ по
- * технологии XIP (Execute-In-Place — выполнение на месте).
+ * Рекомендуется использовать режим XIP, при котором задержка выборки данных из
+ * внешней МФП минимальна, что особенно важно при выполнении из неё программ.
+ * 
+ *            XIP — выполнение на месте (eXecute-In-Place).
  */
