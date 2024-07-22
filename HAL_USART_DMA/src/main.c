24f3508eb93c1abda0bbcada6880277bfa3237b8
Barsy_Barsevich <116821312+Barsy-Barsevich@users.noreply.github.com>
2024-07-22 17:32:11 +0500

    Handler library

--- HAL_USART_DMA/src/main.c
+++ HAL_USART_DMA/src/main.c
@@ -5,24 +5,23 @@
 #include "mik32_hal_usart.h"
 #include "mik32_hal_scr1_timer.h"
 
-#include "xprintf.h"
-
 /*
  * Данный пример демонстрирует возможности передачи данных по интерфейсту USART
  * с использованием DMA.
  *
  * В главном цикле запускается передача данных по DMA, затем в течение времени
- * задержки процессор и DMA конкурируют между собой за доступ к ресурсам памяти
- * и портов микроконтроллера.
+ * задержки DMA передает данные из ОЗУ в USART порт
  */
 
+
+USART_HandleTypeDef husart0;
 DMA_InitTypeDef hdma;
 DMA_ChannelHandleTypeDef hdma_ch0;
 
 static void SystemClock_Config(void);
 static void DMA_CH0_Init(DMA_InitTypeDef *hdma);
 static void DMA_Init(void);
-static void _UART_Init();
+static void USART_Init();
 
 char array[] = "\ntime: ************";
 
@@ -41,7 +40,7 @@ int main()
 
     DMA_Init();
 
-    _UART_Init();
+    USART_Init();
 
     HAL_Time_SCR1TIM_Init();
 
@@ -51,7 +50,6 @@ int main()
         uint32_t time = HAL_Millis();
 
         /* Преобразовать значение времени в строку */
-        //xsprintf(array, "%lu", time);
         utoa(time, array+7, 10);
 
         HAL_DMA_Start(&hdma_ch0, (void*)array, (void*)&UART_0->TXDATA, strlen(array)-1);
@@ -119,48 +117,46 @@ static void DMA_Init(void)
 }
 
 
-static void _UART_Init()
+static void USART_Init()
 {
-    UART_Setting_TypeDef setting;
-
-    setting.Instance = UART_0;
-    setting.transmitting = Enable;
-    setting.receiving = Disable;
-    setting.frame = Frame_8bit;
-    setting.parity_bit = Disable;
-    setting.parity_bit_inversion = Disable;
-    setting.bit_direction = LSB_First;
-    setting.data_inversion = Disable;
-    setting.tx_inversion = Disable;
-    setting.rx_inversion = Disable;
-    setting.swap = Disable;
-    setting.lbm = Disable;
-    setting.stop_bit = StopBit_1;
-    setting.mode = Asynchronous_Mode;
-    setting.xck_mode = XCK_Mode0;
-    setting.last_byte_clock = Disable;
-    setting.overwrite = Disable;
-    setting.cts_processing = Disable;
-    setting.rts_processing = Enable;
-    setting.dma_tx_request = Enable;
-    setting.dma_rx_request = Disable;
-    setting.channel_mode = Duplex_Mode;
-    setting.tx_break_mode = Disable;
-    setting.Interrupt.ctsie = Disable;
-    setting.Interrupt.eie = Disable;
-    setting.Interrupt.idleie = Disable;
-    setting.Interrupt.lbdie = Disable;
-    setting.Interrupt.peie = Disable;
-    setting.Interrupt.rxneie = Disable;
-    setting.Interrupt.tcie = Disable;
-    setting.Interrupt.txeie = Disable;
-    setting.Modem.rts = Disable; //out
-    setting.Modem.cts = Disable; //in
-    setting.Modem.dtr = Disable; //out
-    setting.Modem.dcd = Disable; //in
-    setting.Modem.dsr = Disable; //in
-    setting.Modem.ri = Disable;  //in
-    setting.Modem.ddis = Enable;//out
-    setting.baudrate = 9600;
-    HAL_USART_Init(&setting);
+    husart0.Instance = UART_0;
+    husart0.transmitting = Enable;
+    husart0.receiving = Disable;
+    husart0.frame = Frame_8bit;
+    husart0.parity_bit = Disable;
+    husart0.parity_bit_inversion = Disable;
+    husart0.bit_direction = LSB_First;
+    husart0.data_inversion = Disable;
+    husart0.tx_inversion = Disable;
+    husart0.rx_inversion = Disable;
+    husart0.swap = Disable;
+    husart0.lbm = Disable;
+    husart0.stop_bit = StopBit_1;
+    husart0.mode = Asynchronous_Mode;
+    husart0.xck_mode = XCK_Mode0;
+    husart0.last_byte_clock = Disable;
+    husart0.overwrite = Disable;
+    husart0.rts_mode = AlwaysEnable_mode;
+    /* Разрешить активацию DMA по запросу USART0 */
+    husart0.dma_tx_request = Enable;
+    husart0.dma_rx_request = Disable;
+    husart0.channel_mode = Duplex_Mode;
+    husart0.tx_break_mode = Disable;
+    husart0.Interrupt.ctsie = Disable;
+    husart0.Interrupt.eie = Disable;
+    husart0.Interrupt.idleie = Disable;
+    husart0.Interrupt.lbdie = Disable;
+    husart0.Interrupt.peie = Disable;
+    husart0.Interrupt.rxneie = Disable;
+    husart0.Interrupt.tcie = Disable;
+    husart0.Interrupt.txeie = Disable;
+    husart0.Modem.rts = Disable; //out
+    husart0.Modem.cts = Disable; //in
+    husart0.Modem.dtr = Disable; //out
+    husart0.Modem.dcd = Disable; //in
+    husart0.Modem.dsr = Disable; //in
+    husart0.Modem.ri = Disable;  //in
+    husart0.Modem.ddis = Disable;//out
+    husart0.baudrate = 115200;
+    HAL_USART_Init(&husart0);
 }
