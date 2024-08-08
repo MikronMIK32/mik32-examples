117267ea09b1b7052310b35a2ce166e677c97e98
Barsy_Barsevich <116821312+Barsy-Barsevich@users.noreply.github.com>
2024-08-08 19:54:05 +0500

    Timeouts

--- HAL_USART_IRQ/src/main.c
+++ HAL_USART_IRQ/src/main.c
@@ -1,30 +1,21 @@
-#include "xprintf.h"
 #include "mik32_hal_usart.h"
-#include "mik32_hal_irq.h"
 
-/**
- * Данный пример демонстрирует возможности приема и отправки данных по
- * интерфейсу USART по прирываниям.
+/*
+ * Данный пример демонстрирует возможности приема и передачи данных
+ * по интерфейсу USART.
  * 
- * Программа считывает отправленные по интерфейсу USART строки
- * длины не более BUFFER_LENGTH-1 и отправляет их по USART обратно.
- * 
- * Скорость порта USART - 115200 бод (может быть изменена, см. USART_Init())
+ * При передаче на устройство (микроконтроллер) строки текста, символы
+ * загружаются в буфер, пока не будет обнаружен символ '\n' возврата
+ * каретки. Он заменяется на символ '\0' конца строки, а затем содержимое
+ * буфера печатается функцией HAL_USART_Print.
  */
 
-/* Значение должно быть меньше или равно 254 */
+
+/* Value must be less than 255 */
 #define BUFFER_LENGTH   50
 
 
 USART_HandleTypeDef husart0;
-
-/* Буфер для хранения вводимых данных */
-static char buf[BUFFER_LENGTH];
-/* Указатель на текущий элемент буфера */
-uint8_t buf_pointer;
-/* Флаг готовности принятых данных */
-volatile bool buf_ready = false;
-
 
 void SystemClock_Config(void);
 void USART_Init();
@@ -36,85 +27,26 @@ int main()
 
     USART_Init();
 
-    HAL_USART_Print(&husart0, "Start\n");
+    HAL_USART_Print(&husart0, "Start\n", USART_TIMEOUT_DEFAULT);
 
-    __HAL_PCC_EPIC_CLK_ENABLE();
-    HAL_EPIC_MaskLevelSet(HAL_EPIC_UART_0_MASK); 
-    HAL_IRQ_EnableInterrupts();
+    char buf[BUFFER_LENGTH];
+    uint8_t buf_pointer = 0;
 
-    
     while (1)
     {
-        /* Разрешить прерывания по признаку "регистр приемника не пуст" */
-        HAL_USART_RXNE_EnableInterrupt(&husart0);
-
-        buf_pointer = 0;
-        /* Ожидание приема строки. Вводимая оканчивается символом '\n' */
-        while(!buf_ready);
-
-        /* Запретить прерывания по приему данных */
-        HAL_USART_RXNE_DisableInterrupt(&husart0);
-
-        buf_ready = false;
-        
-        /* Прибавляем к полученной строке символ возврата каретки */
-        strcat(buf, "\n");
-
-        /* Разрешить прерывания по признаку "передача данных завершена" */
-        HAL_USART_TXC_EnableInterrupt(&husart0);
-
-        /* Передача первого байта строки, последующие будут отправляться
-           обработчиком прерываний */
-        buf_pointer = 1;
-        if (buf[0] != '\0') HAL_USART_WriteByte(&husart0, buf[0]);
-
-        /* Задержка */
-        //for (volatile uint32_t i=0; i<1000000; i++);
-        HAL_DelayMs(1000);
-
-        /* Запретить прерывания по передаче данных */
-        HAL_USART_TXC_DisableInterrupt(&husart0);
-    }
-}
-
-
-void trap_handler()
-{
-    if (EPIC_CHECK_UART_0())
-    {
-        /* Прием данных: запись в буфер */
-        if (HAL_USART_RXNE_ReadFlag(&husart0))
+        HAL_USART_Receive(&husart0, buf+buf_pointer, USART_TIMEOUT_DEFAULT);
+        if (buf[buf_pointer] == '\n')
         {
-            buf[buf_pointer] = HAL_USART_ReadByte(&husart0);
-            /* Если принят символ '\n', заменить его на '\0' (символ конца строки) */
-            if (buf[buf_pointer] == '\n')
-            {
-                buf[buf_pointer] = '\0';
-                buf_pointer = 0;
-                buf_ready = true;
-            }
-            else
-            {
-                buf_pointer += 1;
-                if (buf_pointer >= BUFFER_LENGTH) buf_pointer = 0;
-                buf_ready = false;
-            }
-            HAL_USART_RXNE_ClearFlag(&husart0);
+            buf[buf_pointer] = '\0';
+            buf_pointer = 0;
+            HAL_USART_Print(&husart0, buf, USART_TIMEOUT_DEFAULT);
+            HAL_USART_Transmit(&husart0, '\n', USART_TIMEOUT_DEFAULT);
         }
-
-        /* Передача данных: чтение из буфера */
-        if (HAL_USART_TXC_ReadFlag(&husart0))
+        else
         {
-            if (buf[buf_pointer] != '\0')
-            {
-                HAL_USART_WriteByte(&husart0, buf[buf_pointer]);
-                buf_pointer += 1;
-            }
-            else buf_pointer = 0;
-            HAL_USART_TXC_ClearFlag(&husart0);
+            if (++buf_pointer >= BUFFER_LENGTH) buf_pointer = 0;
         }
     }
-   HAL_EPIC_Clear(0xFFFFFFFF);
 }
 
 
